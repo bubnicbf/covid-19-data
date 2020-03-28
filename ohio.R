4c1e55888258781ac78c008dc9021965b633895e
Ben Bubnick <bfbubnic@us.ibm.com>
2020-03-28 08:18:17 -0400

    adding rowth rate plot

--- ohio.R
+++ ohio.R
@@ -23,3 +23,45 @@ counties %>% filter(state=='Ohio') %>%
               values_from = c('cases','deaths')) %>%
   View()
 
+ohio_counties <- counties %>%
+  filter(state=='Ohio')
+
+cases_gt50 <- ohio_counties %>% filter(county %in%
+                                         c(ohio_counties %>%
+                                             group_by(county) %>%
+                                             summarize(n_sum = sum(cases)) %>%
+                                             filter(n_sum >50) %>%
+                                             .$county))
+
+get_xgt50 <- function(x){
+  y <- x%>% filter(county %in%
+                     c(x %>%
+                         group_by(county) %>%
+                         summarize(n_sum = sum(cases)) %>%
+                         filter(n_sum >50) %>%
+                         .$county))
+  return(y)
+}
+
+growth <- function(x)x/(lag(x)-1)
+
+calculate_growth_rate <- function(x){
+  y <- cases_gt50 %>%
+    mutate(date = date) %>%
+    mutate(cumsum = cumsum(cases)) %>%
+    complete( fill = list(cases = 0)) %>%
+    mutate(cum_rolling = rollapplyr(cases, width = 10, FUN = sum, partial = TRUE)) %>%
+    drop_na(cumsum)
+  return(y)
+}
+
+growth_rate_plot<- function(x) {
+  x %>%
+    group_by(county) %>%
+    mutate_each(growth, cases, deaths)  %>% 
+    ggplot() +
+    geom_line(aes(x=date, y = cases, color = county))+
+    facet_wrap(~county) +
+    labs(y='cases growth rate')
+}
+
